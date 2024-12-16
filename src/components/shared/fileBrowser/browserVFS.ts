976008d2475d646783fc6e1e4807b4a6cf5f9cf4
Leonel Mandarino <leonelmandarino@gmail.com>
2024-12-16 15:18:01 -0300

    Add VFS db access

--- src/components/shared/fileBrowser/browserVFS.ts
+++ src/components/shared/fileBrowser/browserVFS.ts
@@ -2,23 +2,40 @@
 // https://github.com/TimboKZ/chonky-website/blob/master/2.x_storybook/src/demos/VFSMutable.tsx
 import { useCallback, useEffect, useRef, useState } from "react";
 import { CustomFileData, CustomFileMap } from "./fileTypes";
-import defaultFiles from "./default_files.json";
 import { NewFileForm } from "./CreateFileDialog";
 import { NewFolderForm } from "./CreateFolderDialog";
+import {
+  createProductFile,
+  deleteProductFiles,
+  getProductFiles,
+  moveProductFiles,
+} from "../../../services/firebase/db";
 
-const prepareCustomFileMap = () => {
-  const baseFileMap = defaultFiles.fileMap as unknown as CustomFileMap;
-  const rootFolderId = defaultFiles.rootFolderId;
-  return { baseFileMap, rootFolderId };
-};
-
-// This hook contains the state of the virtual file system (VFS), with the
+// NOTE: This hook contains the state of the virtual file system (VFS), with the
 // current directory and all the actions the user can perform over files
 export const useBrowserVFS = () => {
-  const { baseFileMap, rootFolderId } = prepareCustomFileMap();
+  const [fileMap, setFileMap] = useState<CustomFileMap>({
+    m9Yj1LnLYwpK4DuXjIDW: {
+      id: "m9Yj1LnLYwpK4DuXjIDW",
+      name: "Loading...",
+      isDir: true,
+      childrenIds: [],
+      childrenCount: 0,
+    },
+  });
+  const [currentFolderId, setCurrentFolderId] = useState("m9Yj1LnLYwpK4DuXjIDW");
 
-  const [fileMap, setFileMap] = useState(baseFileMap);
-  const [currentFolderId, setCurrentFolderId] = useState(rootFolderId);
+  useEffect(() => {
+    async function getFiles() {
+      const files = await getProductFiles();
+      const newFileMap: CustomFileMap = {};
+      for (const doc of files.docs) {
+        newFileMap[doc.id] = { ...doc.data(), id: doc.id } as CustomFileData;
+      }
+      setFileMap(newFileMap);
+    }
+    getFiles();
+  }, []);
 
   const currentFolderIdRef = useRef(currentFolderId);
   useEffect(() => {
@@ -26,6 +43,8 @@ export const useBrowserVFS = () => {
   }, [currentFolderId]);
 
   const deleteFiles = useCallback((files: CustomFileData[]) => {
+    deleteProductFiles(files);
+
     setFileMap((currentFileMap) => {
       // Create a copy of the file map to make sure we don't mutate it.
       const newFileMap = { ...currentFileMap };
@@ -52,6 +71,8 @@ export const useBrowserVFS = () => {
   }, []);
 
   const moveFiles = useCallback((files: CustomFileData[], source: CustomFileData, destination: CustomFileData) => {
+    moveProductFiles(files, source, destination);
+
     setFileMap((currentFileMap) => {
       const newFileMap = { ...currentFileMap };
       const moveFileIds = new Set(files.map((f) => f.id));
@@ -85,56 +106,60 @@ export const useBrowserVFS = () => {
     });
   }, []);
 
-  // TODO: Should replace with UUID generator or similar
-  const idCounter = useRef(0);
-  const createFolder = useCallback((data: NewFolderForm) => {
+  const createFolder = useCallback(async (data: NewFolderForm) => {
+    const folderData = {
+      id: "",
+      name: data.folderName,
+      isDir: true,
+      modDate: new Date(),
+      parentId: currentFolderIdRef.current,
+      childrenIds: [],
+      childrenCount: 0,
+    };
+    const newFolderData = await createProductFile(folderData);
+
     setFileMap((currentFileMap) => {
       const newFileMap = { ...currentFileMap };
 
       // Create the new folder
-      const newFolderId = `new-folder-${idCounter.current++}`;
-      newFileMap[newFolderId] = {
-        id: newFolderId,
-        name: data.folderName,
-        isDir: true,
-        modDate: new Date(),
-        parentId: currentFolderIdRef.current,
-        childrenIds: [],
-        childrenCount: 0,
-      };
+      newFileMap[newFolderData.id] = newFolderData;
 
       // Update parent folder to reference the new folder.
       const parent = newFileMap[currentFolderIdRef.current];
       newFileMap[currentFolderIdRef.current] = {
         ...parent,
-        childrenIds: [...parent.childrenIds!, newFolderId],
+        childrenCount: parent.childrenCount! + 1,
+        childrenIds: [...parent.childrenIds!, newFolderData.id],
       };
 
       return newFileMap;
     });
   }, []);
 
-  const createFile = useCallback((file: NewFileForm) => {
+  const createFile = useCallback(async (file: NewFileForm) => {
+    const fileData = {
+      id: "",
+      name: file.filename + ".txt",
+      isDir: false,
+      modDate: new Date(),
+      parentId: currentFolderIdRef.current,
+      content: { title: file.title, description: file.description },
+      isHidden: false,
+    };
+    const newFileData = await createProductFile(fileData);
+
     setFileMap((currentFileMap) => {
       const newFileMap = { ...currentFileMap };
 
       // Create the new file
-      const newFileId = `new-file-${idCounter.current++}`;
-      newFileMap[newFileId] = {
-        id: newFileId,
-        name: file.filename + ".txt",
-        isDir: false,
-        modDate: new Date(),
-        parentId: currentFolderIdRef.current,
-        content: { title: file.title, description: file.description },
-        isHidden: false,
-      };
+      newFileMap[newFileData.id] = newFileData;
 
       // Update parent folder to reference the new file.
       const parent = newFileMap[currentFolderIdRef.current];
       newFileMap[currentFolderIdRef.current] = {
         ...parent,
-        childrenIds: [...parent.childrenIds!, newFileId],
+        childrenCount: parent.childrenCount! + 1,
+        childrenIds: [...parent.childrenIds!, newFileData.id],
       };
 
       return newFileMap;
