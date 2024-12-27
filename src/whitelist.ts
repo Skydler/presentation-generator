18748735c61bb78b91a22dce0152ccfa7322c95e
Leonel Mandarino <leonelmandarino@gmail.com>
2024-12-26 21:17:02 -0300

    Add new allowed account

--- src/whitelist.ts
+++ src/whitelist.ts
@@ -1,2 +1,2 @@
 // NOTE: Plase don't hack me
-export const userWhitelist = ["xuWMnNkiBXOxhazCiN7Vtdm36dn2"];
+export const userWhitelist = ["xuWMnNkiBXOxhazCiN7Vtdm36dn2", "HhmNsFQwk7ZMpDkvZbQhLNHRWas1"];
