c31abfac607a063af787a0c5396c18b0ce61dca0
Leonel Mandarino <leonelmandarino@gmail.com>
2024-12-31 12:33:49 -0300

    Add division of section components

--- src/components/shared/rte/FixedMenu.tsx
+++ src/components/shared/rte/FixedMenu.tsx
@@ -16,42 +16,73 @@ import {
 } from "react-icons/lu";
 import { FileUploadRoot, FileUploadTrigger } from "../../ui/file-button";
 import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from "../../ui/menu";
+import { Tooltip } from "../../ui/tooltip";
 
 type FixedMenuProps = { editor: Editor };
 export function FixedMenu({ editor }: FixedMenuProps) {
   return (
     <HStack width="full" borderBottom="1px solid #e4e4e7">
-      <IconButton
-        variant={editor.isActive("bold") ? "solid" : "ghost"}
-        size="xs"
-        onClick={() => editor.chain().focus().toggleBold().run()}
-      >
-        <LuBold />
-      </IconButton>
-      <IconButton
-        variant={editor.isActive("italic") ? "solid" : "ghost"}
-        size="xs"
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-      >
-        <LuItalic />
-      </IconButton>
-      <IconButton
-        variant={editor.isActive("underline") ? "solid" : "ghost"}
-        size="xs"
-        onClick={() => editor.chain().focus().toggleUnderline().run()}
-      >
-        <LuUnderline />
-      </IconButton>
-      <IconButton
-        variant={editor.isActive("strike") ? "solid" : "ghost"}
-        size="xs"
-        onClick={() => editor.chain().focus().toggleStrike().run()}
-      >
-        <LuStrikethrough />
-      </IconButton>
+      <MarksSection editor={editor} />
 
       <Separator orientation="vertical" height="20px" />
 
+      <TextAlignSection editor={editor} />
+
+      <MediaSection editor={editor} />
+
+      <Separator orientation="vertical" height="20px" />
+
+      <HistorySection editor={editor} />
+    </HStack>
+  );
+}
+
+function MarksSection({ editor }: { editor: Editor }) {
+  return (
+    <>
+      <Tooltip content="Bold (Ctrl+B)">
+        <IconButton
+          variant={editor.isActive("bold") ? "solid" : "ghost"}
+          size="xs"
+          onClick={() => editor.chain().focus().toggleBold().run()}
+        >
+          <LuBold />
+        </IconButton>
+      </Tooltip>
+      <Tooltip content="Italic (Ctrl+I)">
+        <IconButton
+          variant={editor.isActive("italic") ? "solid" : "ghost"}
+          size="xs"
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+        >
+          <LuItalic />
+        </IconButton>
+      </Tooltip>
+      <Tooltip content="Underline (Ctrl+U)">
+        <IconButton
+          variant={editor.isActive("underline") ? "solid" : "ghost"}
+          size="xs"
+          onClick={() => editor.chain().focus().toggleUnderline().run()}
+        >
+          <LuUnderline />
+        </IconButton>
+      </Tooltip>
+      <Tooltip content="Strikethrough (Ctrl+shift+S)">
+        <IconButton
+          variant={editor.isActive("strike") ? "solid" : "ghost"}
+          size="xs"
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+        >
+          <LuStrikethrough />
+        </IconButton>
+      </Tooltip>
+    </>
+  );
+}
+
+function TextAlignSection({ editor }: { editor: Editor }) {
+  return (
+    <>
       <MenuRoot>
         <MenuTrigger asChild>
           <IconButton variant="ghost" size="xs">
@@ -113,27 +144,35 @@ export function FixedMenu({ editor }: FixedMenuProps) {
       >
         <LuListOrdered />
       </IconButton>
+    </>
+  );
+}
 
-      <FileUploadRoot
-        accept={"image/png, image/jpeg, image/webp"}
-        onFileAccept={(details) => {
-          // Should always be a single file
-          const file = details.files[0];
-          const url = URL.createObjectURL(file);
-          editor.chain().focus().setImage({ src: url }).run();
-        }}
-        allowDrop={false}
-        width="unset"
-      >
-        <FileUploadTrigger asChild>
-          <IconButton variant="ghost" size="xs">
-            <LuImage />
-          </IconButton>
-        </FileUploadTrigger>
-      </FileUploadRoot>
+function MediaSection({ editor }: { editor: Editor }) {
+  return (
+    <FileUploadRoot
+      accept={"image/png, image/jpeg, image/webp"}
+      onFileAccept={(details) => {
+        // Should always be a single file
+        const file = details.files[0];
+        const url = URL.createObjectURL(file);
+        editor.chain().focus().setImage({ src: url }).run();
+      }}
+      allowDrop={false}
+      width="unset"
+    >
+      <FileUploadTrigger asChild>
+        <IconButton variant="ghost" size="xs">
+          <LuImage />
+        </IconButton>
+      </FileUploadTrigger>
+    </FileUploadRoot>
+  );
+}
 
-      <Separator orientation="vertical" height="20px" />
-
+function HistorySection({ editor }: { editor: Editor }) {
+  return (
+    <>
       <IconButton variant="ghost" size="xs" onClick={() => editor.chain().focus().undo().run()}>
         <LuUndo />
       </IconButton>
@@ -141,6 +180,6 @@ export function FixedMenu({ editor }: FixedMenuProps) {
       <IconButton variant="ghost" size="xs" onClick={() => editor.chain().focus().redo().run()}>
         <LuRedo />
       </IconButton>
-    </HStack>
+    </>
   );
 }
